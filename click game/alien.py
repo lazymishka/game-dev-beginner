b7549d806fee46bbad553f8c87104abed06dfb66
lazymishka <vampx1r3s@gmail.com>
2026-01-18 12:58:21 +0100

    Update alien.py

--- click game/alien.py
+++ click game/alien.py
@@ -4,8 +4,20 @@ TITLE="Hit the monster"
 WIDTH=500
 HEIGHT=500
 mon=Actor("monster.png")
+mes = "Hit the monster"
 def draw():
     screen.fill("hot pink")
     mon.draw()
-    screen.draw.text("Hit the monster", center=(250,250), fontsize= 25, color="yellow",fontname="Times New Bd")
+    screen.draw.text(mes, center=(250,250), fontsize= 25, color="yellow")
+#functionforrandposofthemonster#
+def monsterpos():
+    mon.x=random.randint(0,500)
+    mon.y=random.randint(0,500)
+    clock.schedule_unique(misclick,2.0)
+def misclick():
+    global mes
+    mes= "You've misclicked, try again!" 
+    monsterpos()
+
+monsterpos()
 pgzrun.go()
