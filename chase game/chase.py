34219a9df7cda15a7132d90fa9e4eb11ee3ed000
lazymishka <vampx1r3s@gmail.com>
2026-02-13 20:06:06 +0100

    Update chase.py

--- chase game/chase.py
+++ chase game/chase.py
@@ -4,16 +4,24 @@ WIDTH=736
 HEIGHT=414
 monkey = Actor('monkey.png')
 banana = Actor('banana.png')
+score = 0 
 def draw():
     #bgimage
     screen.blit("house.jpg",(0,0))
     monkey.draw()
     banana.draw()
+    #writing score
+    screen.draw.text("Score : "+str(score), color="yellow",center=(35,15))
+    #changescreen
+    if over == True:
+        screen.fill("white")
+        screen.draw.text("Game over!", color="Black",center = (368, 207),fontsize=55)
 #randomposbanana
 def posbanana():
     banana.x=random.randint(0,736)
     banana.y=random.randint(0,414)
 def update():
+    global score
     if keyboard.W:
         monkey.y-=6
     if keyboard.S:
@@ -24,6 +32,14 @@ def update():
         monkey.x+=6
     #monkeytouchingbanana
     if monkey.colliderect(banana):
+        score +=1
         posbanana()
+#game over
+over = False
+def gover():
+    global over
+    over = True
+#scheduling game ovrr function after 60s
+clock.schedule(gover, 60.0)
 posbanana()
 pgzrun.go()
