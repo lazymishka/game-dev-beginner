fab04d8b037020c51e8e69310af1859c14f412ee
lazymishka <vampx1r3s@gmail.com>
2026-03-22 12:58:29 +0100

    Update connect.py

--- connecting game/connect.py
+++ connecting game/connect.py
@@ -25,11 +25,32 @@ def loopcake():
 def draw():
     global totaltime
     screen.blit("baking.jpeg", (0,0))
+    count=1
     for i in listt:
         i.draw()
+        screen.draw.text(str(count),(i.pos[0], i.pos[1]+20))
+        count=count+1
+    for g in cupcakeline:
+        screen.draw.line(g[0],g[1],"black")
     if countcupcake < totalcupcakes:
         totaltime=time.time()-starttime
         screen.draw.text(str(round(totaltime,1)),(15,15))
+    else:
+       screen.draw.text(str(round(totaltime,1)),(15,15)) 
+
+#function for mouseclick
+def on_mouse_down(pos):
+    global countcupcake, cupcakeline
+    if countcupcake < totalcupcakes:
+        if listt[countcupcake].collidepoint(pos):
+            if countcupcake:
+                cupcakeline.append((listt[countcupcake-1].pos, listt[countcupcake].pos))
+            countcupcake=countcupcake+1
+        else:
+            cupcakeline=[]
+            countcupcake=0
+        
+
 def update():
     pass
 
