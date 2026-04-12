81206e522d1f1ee466ac59839b36d3dd66e7ec8c
lazymishka <vampx1r3s@gmail.com>
2026-04-12 12:56:03 +0200

    Update click.py

--- right o game/click.py
+++ right o game/click.py
@@ -3,8 +3,35 @@ import pgzrun
 
 WIDTH=891
 HEIGHT=500
+#gaming variables
+gameover=False
+gamewin=False
+currentlev=1
+totallev=10
+speedob=4
+listtt=["notebook.png", "ruler.png","pencil sharp.png", "rubber.png", "penci].webp"]
+list2=[]
+targett=None
+
 def draw():
     screen.blit("classroom.jpg", (0,0))
 def update():
     pass
-pgzrun.go()
+#function to create falling items
+def crefall(extra):
+    global targett
+    list3=[]
+    targett=random.choice(listtt)
+    wrongitem= random.choices([i for i in listtt if i != targett], k=extra)
+    comblist=[targett]+wrongitem
+    random.shuffle(comblist)
+    sizee=WIDTH/(len(comblist)+1)
+    for u, img in enumerate(comblist):
+        act=Actor(img)
+        act.active=True
+        act.x = (u+1) * sizee
+        act.y = random.randint(-100,0)
+        list3.append(act)
+        #animating
+pgzrun.go() 
+
